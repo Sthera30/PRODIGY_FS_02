17f9387d449a9cbfe40d5bd6a4dfa35448e6ece7
Anelisa Maneli <Anelisamaneli@Katlegos-MacBook-Air-2.local>
2024-11-13 13:45:00 +0200

    Installed bcryptjs on backend

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost:5174'
 }))
 
 app.use(cookieParser())
